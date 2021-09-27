f977f5962b5c56d32ee61869e1c2bb467c3b7ac8
panjun02 <panjun02@meituan.com>
2021-09-27 19:52:58 +0800

    feat: add firebase & shakejs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <router-link to="/filters">Filters</router-link>
+    <div style="height:1px; width:1px; overflow:hidden">
+      <audio src="../assets/mp3/5018.mp3" ref="shakeAudio"></audio>
+      <audio src="../assets/mp3/5012.mp3" ref="shakeResult"></audio>
+    </div>
+    <div class="animation">
+      摇一摇:{{shaked}}<br/>
+      震动: {{vibrate}}<br/>
+    </div>
+    <div>
+      <router-link to="/filters">Filters</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Shake from 'shake.js';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      shaked: 'false',
+      vibrate: 'unknown',
+    };
+  },
   components: {
+  },
+  mounted() {
+    const myShakeEvent = new Shake({
+      threshold: 12, // optional shake strength threshold
+      timeout: 500, // optional, determines the frequency of event generation
+    });
+    myShakeEvent.start();
+    myShakeEvent.hasDeviceMotion = true;
+    window.addEventListener('shake', this.shakeEventDidOccur, false);
+  },
+  methods: {
+    shakeEventDidOccur() {
+      const audio = this.$refs.shakeAudio;
+      this.shaked = 'shake audio';
+      if (window.navigator.vibrate) {
+        navigator.vibrate([500, 200, 500]);
+        this.vibrate = 'support';
+      } else {
+        this.vibrate = 'not support';
+      }
+      audio.play();
+      setTimeout(this.shakeResult, 1000);
+    },
+
+    shakeResult() {
+      this.shaked = 'shaked result';
+      const audio = this.$refs.shakeResult;
+      audio.play();
+    },
   },
 };
 </script>
